9bb5b40a7409629008e220401e8f1da7abc17eb7
Sonny Jordan <maynelson@ideyatech.com>
2017-04-19 13:15:34 +0800

    pom updated

--- src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
+++ src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
@@ -3,23 +3,17 @@ package com.paymaya.ams.amanda.hubspot.adapter.controller;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.paymaya.ams.amanda.hubspot.adapter.dto.form.HubspotWebhookJsonForm;
-import com.paymaya.ams.amanda.hubspot.adapter.dto.form.enums.HubspotCompanyProperty;
+import javax.xml.bind.DatatypeConverter;
 
 @RestController
 @RequestMapping(value = "/hubspot")
@@ -29,20 +23,22 @@ public class SubscriptionController {
 
     @RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Object submitForm(@RequestBody String body, @RequestHeader(name = "X-HubSpot-Signature") String signature) throws NoSuchAlgorithmException {
+    public void submitForm(@RequestBody String body, @RequestHeader(name = "X-HubSpot-Signature") String signature) throws NoSuchAlgorithmException {
     	
-    	 LOGGER.info("body: {}", body);
-
+    	LOGGER.info("body: {}", body);
+    	 
         LOGGER.info("signature: {}", signature);
         
-        String bodyAndSignature = body + signature;
-
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(bodyAndSignature.getBytes());
+        String keyPlusBody = "2edebaad-831e-40ea-9a20-e9c6a48ce18b" + body;
         
-        LOGGER.info("hashed body and signature: {}", hash);
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        
+        byte[] hash = digest.digest(keyPlusBody.getBytes(StandardCharsets.UTF_8));
+        
+        String hello = DatatypeConverter.printHexBinary(hash);
+        
+        LOGGER.info("hashed app secret and body: {}", hello);
 
-        return "hello";
     }
 }
 
