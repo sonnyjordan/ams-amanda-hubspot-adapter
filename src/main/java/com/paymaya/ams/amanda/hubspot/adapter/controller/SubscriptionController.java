77b2a6e945c2ae90b97196c4044bb5f79a87c53e
Sonny Jordan <maynelson@ideyatech.com>
2017-04-19 13:35:10 +0800

    pom updated

--- src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
+++ src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
@@ -7,6 +7,7 @@ import java.security.NoSuchAlgorithmException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,24 +22,38 @@ public class SubscriptionController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SubscriptionController.class);
 
-    @RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    String appSecret = "2edebaad-831e-40ea-9a20-e9c6a48ce18b";
+    
+    @RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseBody
-    public void submitForm(@RequestBody String body, @RequestHeader(name = "X-HubSpot-Signature") String signature) throws NoSuchAlgorithmException {
+    public void submitForm(@RequestBody String body, @RequestHeader(name = "X-HubSpot-Signature") String signature) throws Exception{
+    	
+    	verifySignature(body, signature);
+
+    }
+    
+    private void verifySignature(String body, String signature) throws Exception{
     	
     	LOGGER.info("body: {}", body);
-    	 
+   	 
         LOGGER.info("signature: {}", signature);
         
-        String keyPlusBody = "2edebaad-831e-40ea-9a20-e9c6a48ce18b" + body;
+        String appSecretAndRequestBody =  appSecret + body;
         
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         
-        byte[] hash = digest.digest(keyPlusBody.getBytes(StandardCharsets.UTF_8));
+        byte[] hashedValue = digest.digest(appSecretAndRequestBody.getBytes(StandardCharsets.UTF_8));
         
-        String hello = DatatypeConverter.printHexBinary(hash);
+        String digestValue = DatatypeConverter.printHexBinary(hashedValue);
         
-        LOGGER.info("hashed app secret and body: {}", hello);
-
+        if(!digestValue.equalsIgnoreCase(signature)){
+        	
+        	LOGGER.info("Invalid Request Signature");
+        	
+        	throw new Exception("Invalid Request Signature");
+        }
+        
+        LOGGER.info("Valid Request Signature");
     }
 }
 
