9350798ca616704e5a695e3ec548723abf8c80b3
Sonny Jordan <maynelson@ideyatech.com>
2017-05-17 13:35:56 +0800

    hello

--- src/main/java/com/paymaya/ams/amanda/hubspot/adapter/dto/form/HubspotWebhookJsonForm.java
+++ src/main/java/com/paymaya/ams/amanda/hubspot/adapter/dto/form/HubspotWebhookJsonForm.java
@@ -1,22 +1,60 @@
 package com.paymaya.ams.amanda.hubspot.adapter.dto.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class HubspotWebhookJsonForm {
-	
-	private long eventId;
-	private long subscriptionId;
-	private long portalId;
-	private long appId;
-	private long occurredAt;
-	private String subscriptionType;
-	private long objectId;
-	private String propertyName;
-	private String propertyValue;
-	private String changeSource;
-	private long attemptNumber;
 
+	@JsonProperty("objectId")
+	private Long objectId;
+
+	@JsonProperty("propertyName")
+	private String requestType;
+
+	@JsonProperty("propertyValue")
+	private String requestTypeValue;
+
+	@JsonProperty("changedSource")
+	private String changeSource;
+
+	@JsonProperty("eventId")
+	private Long eventId;
+
+	@JsonProperty("subscriptionId")
+	private Long subscriptionId;
+
+	@JsonProperty("PortalId")
+	private Long portalId;
+
+	@JsonProperty("appId")
+	private Long appId;
+
+	@JsonProperty("occurredAt")
+	private Long occurredAt;
+
+	@JsonProperty("subscriptionType")
+	private String subscriptionType;
+
+	@JsonProperty("attemptNumber")
+	private Long attemptNumber;
+
+	@Override
+	public String toString() {
+
+		return "{\"eventId\":" + "\"" + eventId + "\"," +
+				"\"subscriptionId\":" + "\"" + subscriptionId + "\"," +
+				"\"portalId\":" + "\"" + portalId + "\"," +
+				"\"appId\":" + "\"" + appId + "\"," +
+				"\"occurredAt\":" + "\"" + occurredAt + "\"," +
+				"\"subscriptionType\":" + "\"" + subscriptionType + "\"," +
+				"\"objectId\":" + "\"" + objectId + "\"," +
+				"\"propertyName\":" + "\"" + requestType + "\"," +
+				"\"propertyValue\":" + "\"" + requestTypeValue + "\"," +
+				"\"changeSource\":" + "\"" + changeSource + "\"," +
+				"\"attemptNumber\":" + "\"" + attemptNumber + "\"}";
+	}
 }
