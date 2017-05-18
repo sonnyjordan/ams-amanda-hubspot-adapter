edee63192ca5c5807d6891c331ef272dcb864115
Sonny Jordan <maynelson@ideyatech.com>
2017-05-18 11:20:11 +0800

    m

--- src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
+++ src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
@@ -3,12 +3,10 @@ package com.paymaya.ams.amanda.hubspot.adapter.controller;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,26 +26,21 @@ import javax.xml.bind.DatatypeConverter;
 @RestController
 @RequestMapping(value = "/hubspot")
 public class SubscriptionController {
-	
-	@Value("${hubspot.client.secret}")
-    private String hubspotClientSecretKey;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SubscriptionController.class);
+
+    String appSecret = "6ff686e7-17f3-4e70-9e72-ac44d09601fc";
     
     @RequestMapping(value = "/webhook", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public void submitForm(@RequestBody HubspotWebhookJsonForm [] body, @RequestHeader(name = "X-HubSpot-Signature") String signature) throws Exception{
+    public void submitForm(@RequestBody String body, @RequestHeader(name = "X-HubSpot-Signature") String signature) throws Exception{
     	
-    	
-    	if (!verifySignature(body.toString(), Arrays.toString(body))) {
+    	if (!verifySignature(body, signature)) {
 
-            LOGGER.error("Webhook Request Failed. Invalid signature.");
+            LOGGER.info("Webhook Request Failed. Invalid signature.");
             return;
-            
-        } else {
-            LOGGER.info("VALID SIGNATURE.");
-
         }
+    	
     	
     	
     	
@@ -60,23 +53,25 @@ public class SubscriptionController {
     	
     	
     	
-    	//HubspotWebhookJsonForm [] hubspotWebhookJsonForm = JsonUtil.toObject(HubspotWebhookJsonForm[].class, body);
+    	HubspotWebhookJsonForm [] hubspotWebhookJsonForm = JsonUtil.toObject(HubspotWebhookJsonForm[].class, body);
     	
-    	//LOGGER.info("" + hubspotWebhookJsonForm[0].getAppId());
+    	LOGGER.info("" + hubspotWebhookJsonForm[0].getAppId());
 
     }
     
     private boolean verifySignature(String body, String signature) throws Exception{
     	
     	LOGGER.info("body: {}", body );
-   	         
-        LOGGER.info("-------> Signature: {} ------> ", signature);
+   	 
+        LOGGER.info("signature: {}", signature);
+        
+        LOGGER.info("Signature: {}", signature);
 
-        final String expectedDecryptedValue = hubspotClientSecretKey + body;
+        final String expectedDecryptedValue = appSecret + body;
 
         final String sha256Value = Hashing.sha256().hashString(expectedDecryptedValue, StandardCharsets.UTF_8).toString();
 
-        LOGGER.info("--------> SHA256 Value: {} ---------> ", sha256Value);
+        LOGGER.info("SHA256 Value: {}", sha256Value);
 
         return StringUtils.equals(sha256Value, signature);
         
