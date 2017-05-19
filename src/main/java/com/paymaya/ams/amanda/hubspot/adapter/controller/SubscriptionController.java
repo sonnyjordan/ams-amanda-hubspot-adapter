a5605a6054f42f1a4a0de2ae551c6f5c3813126a
Sonny Jordan <maynelson@ideyatech.com>
2017-05-19 13:04:53 +0800

    m

--- src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
+++ src/main/java/com/paymaya/ams/amanda/hubspot/adapter/controller/SubscriptionController.java
@@ -50,12 +50,6 @@ public class SubscriptionController {
     	
     	LOGGER.info("Converting to HubspotWebhookJsonForm");
     	
-    	
-    	
-    	
-    	HubspotWebhookJsonForm [] hubspotWebhookJsonForm = JsonUtil.toObject(HubspotWebhookJsonForm[].class, body);
-    	
-    	LOGGER.info("" + hubspotWebhookJsonForm[0].getAppId());
 
     }
     
